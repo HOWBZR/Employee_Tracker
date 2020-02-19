f768772149ca482a345f780cdafe089837ffac6a
David Coons <dlc9005@gmail.com>
2020-02-18 21:18:56 -0700

    added more employee functions

--- server.js
+++ server.js
@@ -76,10 +76,10 @@ function startProgram() {
 
 // add employees function
 
-let managerArray = ['Not listed']
+// let managerArray = ['Not listed']
 
 function addEmployee() {
-    const query = 'SELECT * FROM employee WHERE employee.role_id = 1';
+    const query = 'SELECT * FROM employee WHERE employee.role_id IN (1,3,6)';
     connection.query(query, function (err, res) {
         if (err) throw err;
 
@@ -112,14 +112,13 @@ function addEmployee() {
 
             {
                 type: 'list',
-                name: 'manager name',
+                name: 'manager',
                 message: 'Who is their manager?',
                 choices: function () {
                     let managerArray = ['Not listed']
-                    // const employeeId = [];
-                    for (let i = 0; i < res.length; i++) {
-                        managerArray.push(res[i].first_name + ' ' + res[i].last_name + ' ' + res[i].id);
 
+                    for (let i = 0; i < res.length; i++) {
+                        managerArray.push(res[i].first_name + ' ' + res[i].last_name);
                     }
                     return (managerArray);
 
@@ -132,7 +131,9 @@ function addEmployee() {
                             'INSERT INTO employee SET ?', {
                             first_name: answers.first,
                             last_name: answers.last,
-                            role_id: 1
+                            role_id: 1,
+                            manager_name: answers.manager
+
 
                         })
                         break;
@@ -141,7 +142,8 @@ function addEmployee() {
                             'INSERT INTO employee SET ?', {
                             first_name: answers.first,
                             last_name: answers.last,
-                            role_id: 2
+                            role_id: 2,
+                            manager_name: answers.manager
                         })
 
                         break;
@@ -150,7 +152,8 @@ function addEmployee() {
                             'INSERT INTO employee SET ?', {
                             first_name: answers.first,
                             last_name: answers.last,
-                            role_id: 3
+                            role_id: 3,
+                            manager_name: answers.manager
                         })
 
                         break;
@@ -159,7 +162,8 @@ function addEmployee() {
                             'INSERT INTO employee SET ?', {
                             first_name: answers.first,
                             last_name: answers.last,
-                            role_id: 4
+                            role_id: 4,
+                            manager_name: answers.manager
                         })
                         break;
                     case ('Accountant'):
@@ -167,7 +171,8 @@ function addEmployee() {
                             'INSERT INTO employee SET ?', {
                             first_name: answers.first,
                             last_name: answers.last,
-                            role_id: 5
+                            role_id: 5,
+                            manager_name: answers.manager
                         })
                         break;
                     case ('Legal Team-Lead'):
@@ -175,7 +180,8 @@ function addEmployee() {
                             'INSERT INTO employee SET ?', {
                             first_name: answers.first,
                             last_name: answers.last,
-                            role_id: 6
+                            role_id: 6,
+                            manager_name: answers.manager
                         })
                         break;
                     case ('Lawyer'):
@@ -183,26 +189,28 @@ function addEmployee() {
                             'INSERT INTO employee SET ?', {
                             first_name: answers.first,
                             last_name: answers.last,
-                            role_id: 7
+                            role_id: 7,
+                            manager_name: answers.manager
                         })
-                        break;
                 }
                 startProgram();
             })
     })
 }
-//view all employees
+//VIEW ALL EMPLOYEES -------------------------------------------------------------------------------------
 function viewAll() {
     const query = 'SELECT * FROM employee';
+    let newTable = []
     connection.query(query, function (err, res) {
         for (let i = 0; i < res.length; i++) {
-
-            console.table([{ id: res[i].id, first_name: res[i].first_name, last_name: res[i].last_name, role_id: res[i].role_id }])
+            newTable.push({ id: res[i].id, first_name: res[i].first_name, last_name: res[i].last_name, role_id: res[i].role_id })
         }
+        console.table(newTable)
+        startProgram();
     })
 }
 
-//view all employees by dept.
+//VIEW EMPLOYEES BY DEPARTMENT-------------------------------------------------------------------------
 
 function employeeByDept() {
     inquirer
@@ -219,19 +227,22 @@ function employeeByDept() {
                 ]
             }
         ]).then(answers => {
-            const query = 'SELECT employee.role_id, employee.first_name, employee.last_name FROM employee INNER JOIN department ON (employee.role_id = department.id) WHERE (department.name = ?)';
+            const query = 'SELECT department.name, employee.role_id, employee.first_name, employee.last_name FROM employee INNER JOIN department ON (employee.role_id = department.id) WHERE (department.name = ?)';
+            let employeeTable = []
             connection.query(query, [answers.dept], function (err, res) {
                 console.log('There are ' + res.length + ' employees in this dept!')
                 for (let i = 0; i < res.length; i++) {
-                    console.log(
-                        'ID:  ' + res[i].role_id + '  ||  First Name: ' + res[i].first_name + '  ||  Last Name: ' + res[i].last_name
-                    )
+                    employeeTable.push({
+                        first_name: res[i].first_name, last_name: res[i].last_name, Department: res[i].name
+                    })
                 }
+                console.table(employeeTable)
             })
+            startProgram();
         })
 }
 
-// function to remove employee
+// REMOVE EMPLOYEE ----------------------------------------------------------------------------------------
 
 function removeEmployee() {
 
@@ -247,10 +258,8 @@ function removeEmployee() {
                 message: 'Which employee would you like to remove?',
                 choices: function () {
                     const employeeArr = [];
-                    // const employeeId = [];
                     for (let i = 0; i < res.length; i++) {
                         employeeArr.push(res[i].first_name + ' ' + res[i].last_name + ' ' + res[i].id);
-
                     }
                     return (employeeArr);
 
@@ -266,17 +275,103 @@ function removeEmployee() {
                         id: id
 
                     })
+                startProgram();
             })
 
     })
 }
+//UPDATE EMPLOYEE BY MANAGER -------------------------------------------------------------------
+function employeeByManager() {
+    const query = 'SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name, employee.manager_name FROM ((employee INNER JOIN role ON employee.role_id = role.id) INNER JOIN  department ON role.department_id = department.id)'
+    connection.query(query, function (err, res) {
+        for (let i = 0; i < res.length; i++) {
+            console.table([{ id: res[i].id, first_name: res[i].first_name, last_name: res[i].last_name, manager_name: res[i].manager_name }])
+        }
+        startProgram()
+    })
+}
 
-// function managerLookUp() {
+//VIEW EMPLOYEES BY MANAGER----------------------------------------------------------------------------------
+function employeeByManager() {
+    const query = 'SELECT * FROM employee WHERE employee.role_id IN (1,3,6)';
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        inquirer
+            .prompt([
+                {
+                    name: 'manager',
+                    type: 'list',
+                    message: 'Which manager\'s employees do you want to see?',
+                    choices: function () {
+                        let managerArray = []
 
-//     const query = 'SELECT * FROM employee WHERE employee.role = 1 AND employee.role = 3 AND employee.role = 6 ';
-//     connection.query(query, function (err, res) {
+                        for (let i = 0; i < res.length; i++) {
+                            managerArray.push(res[i].first_name + ' ' + res[i].last_name);
+                        }
+                        return (managerArray);
 
-//         if (err) throw err;
-//     })
+                    }
+                }
+            ]).then(answers => {
+                const query = 'SELECT employee.first_name, employee.last_name FROM employee WHERE employee.manager_name = ? ';
+                let listArr = []
+                connection.query(query, [answers.manager], function (err, res) {
+                    console.log(answers.manager + 'has ' + res.length + ' employees reporting to thme, here they are!')
+                    for (let i = 0; i < res.length; i++) {
+                        listArr.push({ first_name: res[i].first_name, last_name: res[i].last_name })
+                    }
+                    console.table(listArr)
+                })
+                startProgram();
+            })
+    })
+}
+// VIEW ALL ROLES ---------------------------------------------------------------------------------------
+function viewAllRoles() {
+    const query = 'SELECT role.title FROM employee INNER JOIN role ON employee.role_id = role.id'
+    let roleList = []
+    connection.query(query, function (err, res) {
+        for (let i = 0; i < res.length; i++) {
+            roleList.push({ Roles: res[i].title })
+        }
+        console.table(roleList)
+        startProgram()
+    })
+}
 
-// }
+//UPDATE EMPLOYEE ROLE ---------------------------------------------------------------------------------
+
+function updateEmployeeRole() {
+    const query = 'SELECT * FROM employee';
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        inquirer
+            .prompt([{
+                type: 'list',
+                name: 'update',
+                message: 'Which employee would you like to update ?',
+                choices: function () {
+                    const employeeArr = [];
+                    for (let i = 0; i < res.length; i++) {
+                        employeeArr.push(res[i].first_name + res[i].last_name);
+                    }
+                    return employeeArr;
+                }
+            },
+            {
+                type: 'list',
+                name: 'title',
+                message: 'What is their new title ?',
+                choices: [
+                    'Sales Lead',
+                    'Sales Person',
+                    'Lead Engineer',
+                    'Software Engineer',
+                    'Accountant',
+                    'Legal Team - Lead',
+                    'Lawyer'
+                ]
+            }
+            ]).then(answers => {
+        let newAnswers = []
+    })
